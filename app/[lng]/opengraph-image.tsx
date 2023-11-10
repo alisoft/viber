313270f247a7f33c7d319bda3456e4a433996b40
kjxbyz <kjxbyz@163.com>
2023-11-10 22:10:00 +0800

    fix: replace all occurrences of `Precedent`

--- app/[lng]/opengraph-image.tsx
+++ app/[lng]/opengraph-image.tsx
@@ -29,7 +29,7 @@ export default async function OG() {
       >
         <img
           src={`${basePath}/logo.jpg`}
-          alt="Precedent Logo"
+          alt="Viber Logo"
           tw="w-20 h-20 mb-4 opacity-95"
         />
         <h1
@@ -44,7 +44,7 @@ export default async function OG() {
             letterSpacing: "-0.02em",
           }}
         >
-          Precedent
+          Viber
         </h1>
       </div>
     ),
