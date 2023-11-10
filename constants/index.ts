cc02546429490c1e5822b0acac53a0d8735da604
kjxbyz <kjxbyz888@outlook.com>
2023-11-10 22:08:32 +0800

    fix: update basePath

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,4 @@
 export const cacheLngKey: string = "__viber_lng__";
 export const cacheThemeKey: string = "__viber_theme__";
+export const basePath =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/viber" : "";
